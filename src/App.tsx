51c2fcb98056caeff9a949392fe3cf6bd9e43263
Deni Cosic <deni.cosic@codeasy.com>
2023-04-13 17:22:55 +0200

    Dodali smo jedan paragraf

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Uvod u git</h1>
       <p>Praćenje promjena u repozitoriju</p>
+      <p>Praćenje promjena u repozitoriju</p>
     </div>
   );
 }
